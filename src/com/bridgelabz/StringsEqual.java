420ac053027700dbea90391246c5332e6624b98a
vinayakgarole <vinayakgarole10@gmail.com>
2022-10-19 16:34:04 +0530

    added: Checking Strings are equal or not

---
+++ src/com/bridgelabz/StringsEqual.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class StringsEqual {
+    public static void main(String[] args) {
+
+        String str1 = ("Hello");
+        String str2 = ("Hello");
+        System.out.println(str1.equals(str2));
+    }
+}
