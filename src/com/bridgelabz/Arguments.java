479f9645a595487fe8e5d4d66af3b0389b83f7f3
vinayakgarole <vinayakgarole10@gmail.com>
2022-10-19 16:14:08 +0530

    added: Sum of Integers of Command lines Arguments

---
+++ src/com/bridgelabz/Arguments.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class Arguments {
+    public static void main(String[] values) {
+
+        int sum = 0;
+        System.out.println("Calculates Sum for below Integers");
+
+        for (int i = 0; i < values.length; i++) {
+            System.out.println(values[i]);
+            sum = sum + Integer.parseInt(values[i]);
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
