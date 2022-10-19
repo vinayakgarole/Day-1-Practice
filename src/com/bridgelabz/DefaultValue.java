219b4dfd2d635533c40fb4e25c87be63aa6ee936
vinayakgarole <vinayakgarole10@gmail.com>
2022-10-19 16:26:40 +0530

    added: Demonstrate Static Variable method and blocks

---
+++ src/com/bridgelabz/DefaultValue.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+public class DefaultValue {
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    char c;
+    boolean bu;
+
+    public static void main(String[] args) {
+
+        DefaultValue def = new DefaultValue();
+        System.out.println(def.b);
+        System.out.println(def.s);
+        System.out.println(def.i);
+        System.out.println(def.l);
+        System.out.println(def.f);
+        System.out.println(def.d);
+        System.out.println(def.c);
+        System.out.println(def.bu);
+    }
+}
