af09a2282c543932ea7cb0efed60449c15a78735
vinayakgarole <vinayakgarole10@gmail.com>
2022-10-19 16:18:04 +0530

    added: Printing name in Command lines Arguments

---
+++ src/com/bridgelabz/CommandLine.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class CommandLine {
+    public static void main(String[] args) {
+
+        Scanner ref= new Scanner(System.in);
+        System.out.print("Write any name here:- ");
+        String s=ref.nextLine();
+        System.out.println("Your given name is:- " +s);
+    }
+}
